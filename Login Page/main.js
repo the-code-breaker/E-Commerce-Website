f2a042c149037636cd15e4b0935dec549e55c40c
Sourav <souravsharma191919@gmail.com>
2022-01-19 18:16:39 +0530

    betterbuys

--- Login Page/main.js
+++ Login Page/main.js
@@ -1,9 +1,26 @@
-function checkform(){
-    if(document.getElementById("name").value == 'jayem30' && document.getElementById("password").value == 'jayem' ){
-        alert("Login Successful");
-        window.location = "http://www.google.com/"
-    }else{
-        document.getElementById('alert').innerHTML = ("Access denied. Valid username and password is required.");
-        
+const loginForm = document.getElementById("myform");
+const loginButton = document.getElementById("submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.name.value;
+    const password = loginForm.password.value;
+
+    if (username === "sourav" ,"abhinav","kartik","himanshu","princesharma" && password === "betterbuys") {
+        document.getElementById('demo1').innerHTML = `" Welcome, You Have Successfully Logged in to BetterBuys" <br> "You are being redirected, please wait...."`;
+        setTimeout(function() {window.location = "../index.html" },3000);
+    } else {
+         document.getElementById('demo2').innerHTML = "Please enter a valid username or password";
+    }
+});
+
+function Toggle() {
+    var temp = document.getElementById("password");
+    if (temp.type === "password") {
+        temp.type = "text";
+    }
+    else {
+        temp.type = "password";
     }
 }
